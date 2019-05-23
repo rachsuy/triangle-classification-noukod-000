17ddc8d5a454e486e0466bb078a79a265e00357f
Rachele Suy <rachelesuy@gmail.com>
2019-05-23 18:32:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,31 @@
 class Triangle
   # write code here
-  
-  attr_accessor :triangle1, :triangle2, :triangle3
-  
-  def initialize(triangle1, triangle2, triangle3)
-    @triangle1 = triangle1
-    @triangle2 = triangle2
-    @triangle3 = triangle3
+  attr_accessor :triangle_sides_1, :triangle_sides_2, :triangle_sides_3
+
+  def initialize(triangle_sides_1, triangle_sides_2, triangle_sides_3)
+      @triangle_sides_1 = triangle_sides_1
+      @triangle_sides_2 = triangle_sides_2
+      @triangle_sides_3 = triangle_sides_3
   end
-  
+
   def kind()
-   if (@triangle_1 <= 0) || (@triangle_2 <= 0) || (@triangle_3 <= 0)
-      raise TriangleError + @triangle_sides_2 <= @triangle_sides
-    elsif (@triangle_side) || 1 || (@triangle_2+@triangle_3 <= @triangle_1)
+    if (@triangle_sides_1 <= 0) || (@triangle_sides_2 <= 0) || (@triangle_sides_3 <= 0)
+      raise TriangleError
+    elsif (@triangle_sides_1 + @triangle_sides_2 <= @triangle_sides_3) || (@triangle_sides_1 + @triangle_sides_3 <= @triangle_sides_2) || (@triangle_sides_2+@triangle_sides_3 <= @triangle_sides_1)
       raise TriangleError
     else
-      if (@triangle_1 == @triangle_2) && (@triangle_2 == @triangle_3)
+      if (@triangle_sides_1 == @triangle_sides_2) && (@triangle_sides_2 == @triangle_sides_3)
         :equilateral
-      elsif (@triangle_1 == @triangle_2) || (@triangle_2 == @triangle_3) || (@triangle_1 == @triangle_3)
+      elsif (@triangle_sides_1 == @triangle_sides_2) || (@triangle_sides_2 == @triangle_sides_3) || (@triangle_sides_1 == @triangle_sides_3)
         :isosceles
-      elsif (@triangle_1 != @triangle_2) && (@triangle_2 != @triangle_3) && (@triangle_1 != @triangle_3)
+      elsif (@triangle_sides_1 != @triangle_sides_2) && (@triangle_sides_2 != @triangle_sides_3) && (@triangle_sides_1 != @triangle_sides_3)
         :scalene
       end
-    end 
+    end
+
   end
-    
-  class TriangleError < StandardError
-    
-  end
+
+class TriangleError < StandardError
+  # triangle error code
+end
 end
